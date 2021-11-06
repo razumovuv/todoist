aac6f58b3433b5367d2a50d54d34dcc37b628013
Yuriy Razumov <razumov.uv@gmail.com>
2021-11-06 10:12:25 +0700

    Update TextField component
    1. Added blureHandler
    2. Added submitHandler
    3. Added isDisabled flag
    4 Added isAutoFocus flag
    5. Added defaultProps for props

--- client/src/components/common/TextField/TextField.js
+++ client/src/components/common/TextField/TextField.js
@@ -10,19 +10,23 @@ import PropTypes from 'prop-types';
 
 export class TextField extends React.Component {
     handleChange = (event) => this.props.changeHandler(event);
-
+    handleBlur = (event) => this.props.blurHandler(event);
+    handleSubmit = (event) => this.props.submitHandler(event);
     render() {
         const className = ['text-field', this.props.customStyle];
         return (
-            <>
-                <input
-                    className={className.join(' ').trim()}
-                    id={this.props.id}
-                    value={this.props.value}
-                    type='text'
-                    onChange={this.handleChange}
-                />
-            </>
+            <input
+                className={className.join(' ').trim()}
+                id={this.props.id}
+                value={this.props.value}
+                type='text'
+                onChange={this.handleChange}
+                disabled={this.props.isDisabled}
+                autoFocus={this.props.isAutoFocus}
+                ref={this.props.inputRef}
+                onBlurCapture={this.handleBlur}
+                onSubmit={this.handleSubmit}
+            />
         );
     }
 }
@@ -32,4 +36,15 @@ TextField.propTypes = {
     value: PropTypes.string,
     customStyle: PropTypes.string,
     changeHandler: PropTypes.func,
+    isDisabled: PropTypes.bool,
+    isAutoFocus: PropTypes.bool,
+    blurHandler: PropTypes.func,
+    submitHandler: PropTypes.func,
+};
+TextField.defaultProps = {
+    changeHandler: () => {},
+    blurHandler: () => {},
+    submitHandler: () => {},
+    isDisabled: false,
+    isAutoFocus: false,
 };
