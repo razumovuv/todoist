bf8be90dc229b1a54d76a71eb905ae7a7acb5f33
Yuriy Razumov <razumov.uv@gmail.com>
2021-11-09 16:31:24 +0700

    Update TODOItem coponent
    It is a differnet component

--- client/src/components/TODOist/TODOList/TODOItem/TODOItem.js
+++ client/src/components/TODOist/TODOList/TODOItem/TODOItem.js
@@ -9,42 +9,92 @@ import PropTypes from 'prop-types';
 //---------> Import components
 import { IconButton } from '@components/common/IconButton';
 import { CheckBox } from '@components/common/CheckBox';
+import { TextField } from '@components/common/TextField';
 
 export class TODOItem extends React.Component {
-    handleEdit = (buttonId) => this.props.editHandler(buttonId);
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentTaskValue: props.children,
+            inputTaskValue: props.children,
+            isEditing: false,
+        };
+        this.taskInputRef = React.createRef();
+    }
     handleDelete = (buttonId) => this.props.deleteHandler(buttonId);
     handleComplite = (event) => this.props.compliteHandler(event);
+    handleEdit = (buttonId) => {
+        this.taskInputRef.current.disabled = false;
+        this.taskInputRef.current.focus();
+        this.setState((state) => {
+            return { inputTaskValue: state.currentTaskValue, isEditing: true };
+        });
+    };
+    handleInputBlur = (event) => {
+        this.taskInputRef.current.disabled = true;
+        this.setState({
+            isEditing: false,
+        });
+    };
+
+    handleInputChange = (event) => {
+        this.setState({ inputTaskValue: event.target.value });
+    };
+    handleUpdate = (event) =>
+        this.props.updateHandler(event, (todo) => {
+            this.setState({ currentTaskValue: todo });
+            this.taskInputRef.current.blur();
+        });
 
     render() {
         return (
-            <div className='todo-list__item'>
-                <li id={this.props.id}>
-                    <div className='todo-list__task'>
+            <li id={this.props.id}>
+                <div className='todo-list__item'>
+                    <div className='todo-list__item__task'>
                         <CheckBox
                             id={'check_' + this.props.id}
                             label={this.props.children}
                             checked={this.props.complited ? 'checked' : ''}
                             changeHandler={this.handleComplite}
-                        />
+                        >
+                            <form
+                                id={'formInput_' + this.props.id}
+                                onSubmit={this.handleUpdate}
+                            >
+                                <TextField
+                                    id={'task_' + this.props.id}
+                                    value={
+                                        this.state.isEditing
+                                            ? this.state.inputTaskValue
+                                            : this.state.currentTaskValue
+                                    }
+                                    isDisabled={true}
+                                    inputRef={this.taskInputRef}
+                                    focusHandler={this.handleInputFocus}
+                                    blurHandler={this.handleInputBlur}
+                                    changeHandler={this.handleInputChange}
+                                />
+                            </form>
+                        </CheckBox>
                     </div>
-                    <div className='todo-list__btn-panel'>
+                    <div className='todo-list__item__btn-panel'>
                         <IconButton
                             id={'editBtn_' + this.props.id}
                             type='Edit'
-                            size='28px'
+                            size='25px'
                             color='#5c8bf0'
                             clickHandler={this.handleEdit}
                         />
                         <IconButton
                             id={'deleteBtn_' + this.props.id}
                             type='Delete'
-                            size='28px'
+                            size='25px'
                             color='red'
                             clickHandler={this.handleDelete}
                         />
                     </div>
-                </li>
-            </div>
+                </div>
+            </li>
         );
     }
 }
@@ -52,7 +102,8 @@ export class TODOItem extends React.Component {
 TODOItem.propTypes = {
     id: PropTypes.string,
     complited: PropTypes.bool,
+    isDisabled: PropTypes.bool,
     deleteHandler: PropTypes.func,
-    editHandler: PropTypes.func,
     compliteHandler: PropTypes.func,
+    updateHandler: PropTypes.func,
 };
